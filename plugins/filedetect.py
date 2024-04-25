4d985679851cb7791df0891d1ecf0f6432532d6e
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2024-04-25 23:20:42 +0530

    Update filedetect.py

--- plugins/filedetect.py
+++ plugins/filedetect.py
@@ -1,44 +1,165 @@
 from pyrogram import Client, filters
-from pyrogram.types import (
-    InlineKeyboardButton, InlineKeyboardMarkup, ForceReply)
+from pyrogram.enums import MessageMediaType
+from pyrogram.errors import FloodWait
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
 
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+
+from helper.utils import progress_for_pyrogram, convert, humanbytes
+from helper.database import db
+
+from asyncio import sleep
+from PIL import Image
+import os, time
+
+# Create a Pyrogram client
+client = Client("my_bot")
+
+# Handler for incoming messages containing documents, audio, or video files
+@Client.on_message(filters.private & (filters.document | filters.audio | filters.video))
+async def rename_start(client, message):
+    # Get the file from the message
+    file = getattr(message, message.media.value)
+    filename = file.file_name  
+
+    # Check if file size is greater than 2GB
+    if file.file_size > 2000 * 1024 * 1024:
+         return await message.reply_text("Sorry, this bot doesn't support uploading files bigger than 2GB")
+
+    try:
+        # Prompt user to enter a new filename
+        await message.reply_text(
+            text=f"Please enter a new filename...\n\nOld File Name: `{filename}`",
+            reply_to_message_id=message.id,  
+            reply_markup=ForceReply(True)
+        )       
+        await sleep(30)
+    except FloodWait as e:
+        await sleep(e.value)
+        await message.reply_text(
+            text=f"Please enter a new filename...\n\nOld File Name: `{filename}`",
+            reply_to_message_id=message.id,  
+            reply_markup=ForceReply(True)
+        )
+    except:
+        pass
+
+# Handler for user replies to prompt for renaming
 @Client.on_message(filters.private & filters.reply)
 async def refunc(client, message):
-    if (message.reply_to_message.reply_markup) and isinstance(message.reply_to_message.reply_markup, ForceReply):
-        new_name = message.text
-        await message.delete()
-        media = await client.get_messages(message.chat.id, message.reply_to_message.id)
-        file = media.reply_to_message.document or media.reply_to_message.video or media.reply_to_message.audio
-        filename = file.file_name
-        types = file.mime_type.split("/")
-        mime = types[0]
-        mg_id = media.reply_to_message.id
-        try:
-            out = new_name.split(".")
-            out[1]
-            out_name = out[-1]
-            out_filename = new_name
-            await message.reply_to_message.delete()
-            if mime == "video":
-                markup = InlineKeyboardMarkup([[
-                    InlineKeyboardButton("📁 Document", callback_data="doc"),
-                    InlineKeyboardButton("🎥 Video", callback_data="vid")]])
-            elif mime == "audio":
-                markup = InlineKeyboardMarkup([[InlineKeyboardButton(
-                    "📁 Document", callback_data="doc"), InlineKeyboardButton("🎵 audio", callback_data="aud")]])
+    # Get the replied message
+    reply_message = message.reply_to_message
+
+    # Check if the replied message contains a ForceReply markup
+    if (reply_message.reply_markup) and isinstance(reply_message.reply_markup, ForceReply):
+        new_name = message.text 
+        await message.delete() 
+        msg = await client.get_messages(message.chat.id, reply_message.id)
+        file = msg.reply_to_message
+        media = getattr(file, file.media.value)
+        if not "." in new_name:
+            if "." in media.file_name:
+                extn = media.file_name.rsplit('.', 1)[-1]
             else:
-                markup = InlineKeyboardMarkup(
-                    [[InlineKeyboardButton("📁 Document", callback_data="doc")]])
-            # Check if new_name is empty or contains only whitespace
-            if new_name.strip():
-                await message.reply_text(f"**Select the output file type**\n**Output FileName** :- ```{out_filename}```", reply_to_message_id=mg_id, reply_markup=markup)
-            else:
-                await message.reply_text("Error: Please provide a valid file name.", reply_to_message_id=mg_id)
-        except:
-            try:
-                out = filename.split(".")
-                out_name = out[-1]
-                out_filename = new_name + "." + out_name
-            except:
-                await message.reply_to_message.delete()
-               
+                extn = "mkv"
+            new_name = new_name + "." + extn
+        await reply_message.delete()
+
+        button = [[InlineKeyboardButton("📁 Document",callback_data = "upload_document")]]
+        if file.media in [MessageMediaType.VIDEO, MessageMediaType.DOCUMENT]:
+            button.append([InlineKeyboardButton("🎥 Video", callback_data = "upload_video")])
+        elif file.media == MessageMediaType.AUDIO:
+            button.append([InlineKeyboardButton("🎵 Audio", callback_data = "upload_audio")])
+        await message.reply(
+            text=f"Select the output file type\nFile Name: `{new_name}`",
+            reply_to_message_id=file.id,
+            reply_markup=InlineKeyboardMarkup(button)
+        )
+
+# Handler for callback queries
+@Client.on_callback_query(filters.regex("upload"))
+async def doc(bot, update):    
+    new_name = update.message.text
+    new_filename = new_name.split(":-")[1]
+    file_path = f"downloads/{new_filename}"
+    file = update.message.reply_to_message
+
+    ms = await update.message.edit("Trying to download...")    
+    try:
+        path = await bot.download_media(message=file, file_name=file_path, progress=progress_for_pyrogram, progress_args=("Download Started....", ms, time.time()))                    
+    except Exception as e:
+        return await ms.edit(e)
+    
+    duration = 0
+    try:
+        metadata = extractMetadata(createParser(file_path))
+        if metadata.has("duration"):
+           duration = metadata.get('duration').seconds
+    except:
+        pass
+    ph_path = None
+    user_id = int(update.message.chat.id) 
+    media = getattr(file, file.media.value)
+    c_caption = await db.get_caption(update.message.chat.id)
+    c_thumb = await db.get_thumbnail(update.message.chat.id)
+
+    if c_caption:
+         try:
+             caption = c_caption.format(filename=new_filename, filesize=humanbytes(media.file_size), duration=convert(duration))
+         except Exception as e:
+             return await ms.edit(text=f"Your Caption Error. Except Keyword Argument -> ({e})")             
+    else:
+         caption = f"{new_filename}"
+ 
+    if (media.thumbs or c_thumb):
+         if c_thumb:
+             ph_path = await bot.download_media(c_thumb) 
+         else:
+             ph_path = await bot.download_media(media.thumbs[0].file_id)
+         Image.open(ph_path).convert("RGB").save(ph_path)
+         img = Image.open(ph_path)
+         img.resize((320, 320))
+         img.save(ph_path, "JPEG")
+
+    await ms.edit("Trying to upload...")
+    type = update.data.split("_")[1]
+    try:
+        if type == "document":
+            await bot.send_document(
+                update.message.chat.id,
+                document=file_path,
+                thumb=ph_path, 
+                caption=caption, 
+                progress=progress_for_pyrogram,
+                progress_args=("Upload Started....", ms, time.time()))
+        elif type == "video": 
+            await bot.send_video(
+                update.message.chat.id,
+                video=file_path,
+                caption=caption,
+                thumb=ph_path,
+                duration=duration,
+                progress=progress_for_pyrogram,
+                progress_args=("Upload Started....", ms, time.time()))
+        elif type == "audio": 
+            await bot.send_audio(
+                update.message.chat.id,
+                audio=file_path,
+                caption=caption,
+                thumb=ph_path,
+                duration=duration,
+                progress=progress_for_pyrogram,
+                progress_args=("Upload Started....", ms, time.time()))
+    except Exception as e:          
+        os.remove(file_path)
+        if ph_path:
+            os.remove(ph_path)
+        return await ms.edit(f"Error {e}")
+ 
+    await ms.delete() 
+    os.remove(file_path) 
+    if ph_path: os.remove(ph_path)
+
+# Run the client
+client.run()
